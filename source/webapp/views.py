817dbb391d53b6fd02bce3d354a5189417ce1660
karamoldoevee <karamoldoevee@gmail.com>
2020-03-14 10:33:26 +0600

    Добавлен CRUD для File и Status.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from webapp.models import File
+from webapp.forms import FileForm
+
+
+class FileListView(ListView):
+    model = File
+    template_name = 'file/list.html'
+    context_object_name = 'files'
+
+    ordering = ['-created_at']
+
+    paginate_by = 10
+
+    paginate_orphans = 1
+
+
+class FileCreateView(CreateView):
+    model = File
+    template_name = 'add.html'
+    form_class = FileForm
+
+    def form_valid(self, form):
+        self.object = File.objects.create(author=self.request.user, upload=form.cleaned_data['upload'],
+                                          signature=form.cleaned_data['signature'],
+                                          created_at=form.cleaned_data['created_at'])
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('webapp:index')
+
+
+class PhotoUpdateView(UpdateView):
+    model = File
+    template_name = 'change.html'
+    fields = FileForm
+    context_object_name = 'file'
+
+    def get_success_url(self):
+        return reverse('webapp:index')
+
+
+class PhotoDeleteView(DeleteView):
+    template_name = 'delete.html'
+    model = File
+    context_object_name = 'file'
+    success_url = reverse_lazy('webapp:index')
